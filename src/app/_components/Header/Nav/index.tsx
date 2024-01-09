18989fb87a6534509b24b1065eff97efa7504e56
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-01-09 11:36:52 +0800

    updated to remove additional cart button

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -12,17 +12,27 @@ import { CMSLink } from '../../Link'
 import classes from './index.module.scss'
 
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
+  // Extracting navItems and user from props and context
   const navItems = header?.navItems || []
   const { user } = useAuth()
 
   return (
+    // Applying conditional styling based on the user's login status
     <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
+      {/* Rendering CMSLinks based on navItems */}
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="none" />
       })}
+
+      {/* Always rendering a single CartLink */}
       <CartLink />
-      {user && <Link href="/account">Account</Link>}
-      {!user && (
+
+      {/* Conditional rendering based on user's login status */}
+      {user ? (
+        // Rendering Account link if user is logged in
+        <Link href="/account">Account</Link>
+      ) : (
+        // Rendering Login button if user is not logged in
         <Button
           el="link"
           href="/login"
@@ -31,7 +41,6 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
           onClick={() => (window.location.href = '/login')}
         />
       )}
-      {user && <CartLink />}
     </nav>
   )
 }
